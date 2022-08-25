ac49038308089952258fa16709d1715723e1df5a
Abhi Nandan <nandan645@protonmail.com>
2022-08-25 22:46:16 +0530

    Update setup-termux.sh

--- setup-termux.sh
+++ setup-termux.sh
@@ -6,6 +6,8 @@ apt update && apt upgrade -y
 
 # Storage Permission---------------------------
 termux-setup-storage
+# Wait for permission -------------------------
+sleep 5s # Waits 5 seconds.
 # Ls into storage -----------------------------
 ls ~/storage/shared
 #----------------------------------------------
