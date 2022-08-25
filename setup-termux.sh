80f052c6a11b5273efc37f07f10f28b77943b112
Abhi Nandan <nandan645@protonmail.com>
2022-08-25 23:18:49 +0530

    Update setup-termux.sh

--- setup-termux.sh
+++ setup-termux.sh
@@ -1,13 +1,25 @@
 #!/bin/bash
 
-# Update System -------------------------------
-apt update && apt upgrade -y
+# Install Python-------------------------------
+pkg install python
+# Upgrade pip
+pip install --upgrade pip
 #----------------------------------------------
+
+# Install yt-dlp
+python3 -m pip install -U yt-dlp
+# Install gallery-dl
+python3 -m pip install -U gallery-dl
+# Installing aria2 & ffmpeg
+pkg install aria2 ffmpeg
+
+# Setting up bashrc-profile
+cp configs/.bash_profile ~
 
 # Storage Permission---------------------------
 termux-setup-storage
-# Wait for permission -------------------------
+# Wait for permission
 sleep 5s # Waits 5 seconds.
-# Ls into storage -----------------------------
+# Ls into storage
 ls ~/storage/shared
 #----------------------------------------------
